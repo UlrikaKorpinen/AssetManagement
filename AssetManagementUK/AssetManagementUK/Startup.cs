8b1c309564340b883cf2704b756288c344f46ce9
UlrikaKorpinen <ulrika.korpinen@stud.pointcollege.fi>
2017-04-25 14:57:30 +0300

    ASP.NET MVC projektin luonti

---
+++ AssetManagementUK/AssetManagementUK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AssetManagementUK.Startup))]
+namespace AssetManagementUK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
